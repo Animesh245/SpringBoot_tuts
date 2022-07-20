8f832e8c53094d236417a421fe11e3a5a819602d
Animesh245 <bdanimesh245@gmail.com>
2022-07-20 22:53:38 +0600

    initial commit

---
+++ social_medium_springboot/src/test/java/com/animesh245/social_medium_springboot/SocialMediumSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh245.social_medium_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediumSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
