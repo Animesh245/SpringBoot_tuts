917800509ad845363ae519483a4a0ffa6f65418b
Animesh245 <bdanimesh245@gmail.com>
2022-07-20 22:55:27 +0600

    module applicaiton main class

---
+++ social_medium_springboot/bs-userinfo/src/main/java/com/animesh245/bsuserinfo/BsUserinfoApplication.java
@@ -0,0 +1,13 @@
+package com.animesh245.bsuserinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsUserinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsUserinfoApplication.class, args);
+	}
+
+}
