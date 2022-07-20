534f194d5083b3f0870bd66be05ce04aad4355c9
Animesh245 <bdanimesh245@gmail.com>
2022-07-20 22:55:13 +0600

    parent applicaiton main class

---
+++ social_medium_springboot/src/main/java/com/animesh245/social_medium_springboot/SocialMediumSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.animesh245.social_medium_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediumSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediumSpringbootApplication.class, args);
+	}
+
+}
