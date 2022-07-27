c80a838103ebc8ca8ebdaf7d46488443b05d22d9
Animesh245 <bdanimesh245@gmail.com>
2022-07-28 02:10:43 +0600

    Backend Module

---
+++ Frontend-maven-plugin/backend/src/main/java/com/animesh245/frontendmavenplugin/FrontendMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package com.animesh245.frontendmavenplugin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendMavenPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendMavenPluginApplication.class, args);
+    }
+
+}
---
+++ Frontend-maven-plugin/backend/src/test/java/com/animesh245/frontendmavenplugin/FrontendMavenPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animesh245.frontendmavenplugin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendMavenPluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
